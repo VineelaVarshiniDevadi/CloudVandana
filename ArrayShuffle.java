dc17d680ef97de0c5ec92f72956f4d4b33b13b87
VineelaVarshiniDevadi <147845612+VineelaVarshiniDevadi@users.noreply.github.com>
2023-10-14 23:15:44 +0530

    Update ArrayShuffle.java

---
+++ ArrayShuffle.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ArrayShuffle {
+    public static void main(String[] args) {
+        // Create array
+        Integer[] array = {1, 2, 3, 4, 5, 6, 7};
+
+        // Convert array to list for shuffling
+        List<Integer> list = new ArrayList<>(Arrays.asList(array));
+
+        // Shuffle the list
+        Collections.shuffle(list);
+
+        // Convert the list back to array
+        array = list.toArray(new Integer[0]);
+
+        // Print the shuffled array
+        System.out.println(Arrays.toString(array));
+    }
+}
